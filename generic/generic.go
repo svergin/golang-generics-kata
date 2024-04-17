b893558f5afef3e80f7bd2c92a80bdba818a91a9
Sven Vergin <sven.vergin@nortal.com>
2024-04-17 11:32:25 +0200

    made all functions variadic

--- generic/generic.go
+++ generic/generic.go
@@ -2,7 +2,7 @@ package generic
 
 import "golang.org/x/exp/constraints"
 
-func Filter2[T constraints.Ordered](elements []T, f func(T) bool, f2 ...func(T) any) []T {
+func Filter[T constraints.Ordered](elements []T, f func(T) bool, otherOps ...func(T) any) []T {
 	res := make([]T, 0)
 	for _, v := range elements {
 		if f(v) {
@@ -12,17 +12,7 @@ func Filter2[T constraints.Ordered](elements []T, f func(T) bool, f2 ...func(T) 
 	return res
 }
 
-func Filter[T constraints.Ordered](elements []T, filter func(T) bool) []T {
-	res := make([]T, 0)
-	for _, v := range elements {
-		if filter(v) {
-			res = append(res, v)
-		}
-	}
-	return res
-}
-
-func Map[T, C any](input []T, mapping func(T) C) []C {
+func Map[T, C any](input []T, mapping func(T) C, otherOps ...func(T) any) []C {
 	res := make([]C, 0)
 	for _, v := range input {
 		res = append(res, mapping(v))
@@ -30,7 +20,7 @@ func Map[T, C any](input []T, mapping func(T) C) []C {
 	return res
 }
 
-func Reduce[T, R any](elements []T, aggregate func(T, R) R) R {
+func Reduce[T, R any](elements []T, aggregate func(T, R) R, otherOps ...func(T) any) R {
 	var result R
 	for _, v := range elements {
 		result = aggregate(v, result)
